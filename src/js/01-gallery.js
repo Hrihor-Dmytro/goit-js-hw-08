38e8c0808d3b2acf03dfd1024388c3658a43f637
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2022-09-03 01:54:58 +0300

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createPhotoCards(el) {
+  return el
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}"/>
+                </a>`;
+    })
+    .join('');
+}
+const cardEl = createPhotoCards(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', cardEl);
+galleryEl.addEventListener('click', noPreventDefault);
+
+function noPreventDefault(evt) {
+  evt.preventDefault();
+}
+
+const litebox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
+
+console.log(litebox);
